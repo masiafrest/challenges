dcd1f4f3d164072bd1cb0145e3cd831d2bc95f95
Jzz <29693812+masiafrest@users.noreply.github.com>
2023-01-28 10:13:15 -0500

    add react testing lib and config file to vite

--- fx-exchange-test/vite.config.ts
+++ fx-exchange-test/vite.config.ts
@@ -6,6 +6,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     environment: 'jsdom',
+    setupFiles: './tests/setup.ts'
   },
 })
